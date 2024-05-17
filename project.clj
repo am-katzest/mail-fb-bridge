c1f0973cc223d12447f81e64c85cc8554b84a82f
bestestest kitteh <236551@edu.p.lodz.pl>
2024-05-18 00:23:01 +0200

    initial

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject email_fb_relay "0.1.0-SNAPSHOT"
+  :description "reads emails, post them on facebook"
+  :url "http://example.com/FIXME"
+  :license {:name "AGPLv3" }
+  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :repl-options {:init-ns email-fb-relay.core})
---
+++ src/email_fb_relay/core.clj
@@ -0,0 +1,6 @@
+(ns email-fb-relay.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
