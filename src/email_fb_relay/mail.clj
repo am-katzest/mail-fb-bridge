12e68ed1e2aa0af872edc8f908866435d3c1839e
bestestest kitteh <236551@edu.p.lodz.pl>
2024-05-18 22:18:53 +0200

    make it partially email provider agnostic

--- src/email_fb_relay/mail.clj
+++ src/email_fb_relay/mail.clj
@@ -1,12 +1,11 @@
 (ns email-fb-relay.mail
   (:require [clojure-mail.core :refer :all]
-            [clojure-mail.gmail :as gmail]
             [clojure-mail.events :as events]
             [clojure-mail.message :refer (read-message)]))
 
-(defn start-manager [{:keys [email password]} f]
+(defn start-manager [{:keys [email password server]} f]
   (let [s (get-session "imaps")
-        gstore (store "imaps" s "imap.gmail.com" email password)
+        gstore (store "imaps" s server email password)
         folder (open-folder gstore "inbox" :readonly)
         im (events/new-idle-manager s)]
     (events/add-message-count-listener (fn [e]
