83a765c6b862efcfe324303ec2f4eb4196254d0e
bestestest kitteh <236551@edu.p.lodz.pl>
2024-05-18 22:25:27 +0200

    move store creation out of start-manager

--- src/email_fb_relay/mail.clj
+++ src/email_fb_relay/mail.clj
@@ -3,15 +3,15 @@
             [clojure-mail.events :as events]
             [clojure-mail.message :refer (read-message)]))
 
-(defn start-manager [{:keys [email password server]} f]
+(defn make-session [{:keys [email password server]}]
   (let [s (get-session "imaps")
-        gstore (store "imaps" s server email password)
+        gstore (store "imaps" s server email password)]
+    [s gstore]))
+
+(defn start-manager [conf f]
+  (let [[s gstore] (make-session conf)
         folder (open-folder gstore "inbox" :readonly)
-        im (events/new-idle-manager s)]
-    (events/add-message-count-listener (fn [e]
-                                         (future (doseq [m (:messages e)]
-                                                   (f (read-message m)))))
-                                       (constantly nil)
-                                       folder
-                                       im)
+        im (events/new-idle-manager s)
+        callback (fn [e] (future (doseq [m (:messages e)] (f (read-message m)))))]
+    (events/add-message-count-listener callback (constantly nil) folder im)
     #(events/stop im)))
