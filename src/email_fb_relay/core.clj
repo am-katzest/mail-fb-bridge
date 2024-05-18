605e56ef1a510f135b2cb589837bbd12ca1c3236
bestestest kitteh <236551@edu.p.lodz.pl>
2024-05-18 23:19:55 +0200

    + email processing

--- src/email_fb_relay/core.clj
+++ src/email_fb_relay/core.clj
@@ -1,6 +1,7 @@
 (ns email-fb-relay.core
   (:require [email-fb-relay.fb :as fb]
             [email-fb-relay.mail :as mail]
+            [clojure.string :as str]
             [aero.core :as aero]))
 
 (defn PA? [msg]
@@ -9,3 +10,14 @@
   (->> msg :subject (re-matches #".*: INITIAL$")))
 
 (def config (aero/read-config "config.edn"))
+
+(defn transformPA [msg]
+  (let [body (-> msg :body :body)
+        content (str/trim (first (str/split-lines body)))]
+    (assert (= 7 (count (str/split-lines body))) "wrong line count")
+    (assert (= 1 (count (re-seq #"https" content))))
+    (let [[[_ message url]] (re-seq #"^(.*) +Visit +(https://511PA\.com.*)$" content)]
+      (assert (and (some? message) (some? url)) "can't split into body & url")
+      {:message (str/trim message)
+       :link url
+       :caption "more details at source"})))
