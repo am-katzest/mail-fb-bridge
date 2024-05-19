3fdf59403d90703700d0f2868422c1f6e890689e
bestestest kitteh <236551@edu.p.lodz.pl>
2024-05-19 09:56:41 +0200

    tolerate faliure to start im (aka. don't crash when network goes away)

--- src/email_fb_relay/mail.clj
+++ src/email_fb_relay/mail.clj
@@ -14,7 +14,7 @@
         folder (open-folder gstore "inbox" :readonly)
         im (events/new-idle-manager s)
         callback (fn [e]
-                   (log/info "received mail!")
+                   (log/infof "received mail! (%d)" (count (:messages e)))
                    (future (doseq [m (:messages e)]
                              (f (read-message m))))
                    (.watch im folder))]
@@ -27,10 +27,22 @@
         (if (.isRunning im) (recur)
             (log/error "idle manager is not running"))))
 
+(defn- try-starting-manager-until-it-works [conf f]
+  (log/info "trying to start a new im")
+  (loop [time 100]
+    (let [im (try (let [im (start-manager conf f)]
+                    (log/info "started new im successfully")
+                    im)
+                  (catch Throwable t
+                    (log/error t "failed to start new im")))]
+      (or im
+          (do
+            (Thread/sleep time)
+            (recur (min 600000 (* time 2))))))))
+
 (defn start-persistant-manager [conf f]
   (loop []
-    (log/info "starting new idle manager")
-    (let [im (start-manager conf f)]
+    (let [im (try-starting-manager-until-it-works conf f)]
       (wait-until-im-stops-running im)
       (recur))))
 
